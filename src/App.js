c07f7cbeda76a2db25b57de942f169e229315fdd
cirocki <marcincirocki89@gmail.com>
2020-01-23 13:18:58 +0100

    fetch API configuration

--- src/App.js
+++ src/App.js
@@ -1,24 +1,38 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
+  const [city, setCity] = useState("Sopot");
+
+  // API VARIABLES
+  const apiKey = "17a58d62db18d4635698028c2b6fe349";
+  const apiCall = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+
+  // FETCH DATA FROM WEATHERMAP API
+  useEffect(() => {
+    const fetchData = () => {
+      fetch(apiCall)
+        .then(res => {
+          if (res.ok) {
+            return res;
+          } else {
+            throw Error("something went wrong");
+          }
+        })
+        .then(res => res.json())
+        .then(res => console.log(res))
+        .catch(error => {
+          console.log("Error, " + error.message);
+        });
+    };
+
+    fetchData();
+  }, [apiCall]);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Weather app</h1>
     </div>
   );
 }
